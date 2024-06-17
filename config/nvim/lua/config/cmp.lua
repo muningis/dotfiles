ea1ba7e655f070aff7fd05eeb0af97c1dfc8d0b2
Rokas Muningis <28229273+muningis@users.noreply.github.com>
2024-06-17 22:02:34 +0300

    fix: mappings -> mapping for nvim-cmp, to fix custom mappings

--- config/nvim/lua/config/cmp.lua
+++ config/nvim/lua/config/cmp.lua
@@ -42,7 +42,7 @@ local options = {
     end
   },
  
-  mappings = {
+  mapping = {
     ["<C-p>"] = cmp.mapping.select_prev_item(),
     ["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-Space>"] = cmp.mapping.complete(),
